eec7512c850c49e9c63011bc38dd5658e7c705f0
Alfaaz Ahmed <alfaazahmed010@gmail.com>
2026-04-27 23:48:47 +0600

    implement google and github authentication

--- src/components/homepage/news/RightSidebar.jsx
+++ src/components/homepage/news/RightSidebar.jsx
@@ -1,17 +1,39 @@
+'use client'
+import { authClient } from '@/lib/auth-client';
 import React from 'react';
 import { FaGithub, FaGoogle } from 'react-icons/fa';
 
 const RightSidebar = () => {
+    const handleGoogleSignin = async () => {
+        const data = await authClient.signIn.social({
+            provider: "google",
+        });
+        console.log(data, "data");
+    }
+
+    const handleGithubSignin = async () => {
+        const data = await authClient.signIn.social({
+            provider: "github",
+        });
+        console.log(data, "data");
+    }
+
     return (
         <div>
             <h4 className="font-medium text-xl mb-4">Social Icons</h4>
             <div>
-                <button className='flex items-center justify-center gap-2 border-2 border-blue-300 text-blue-400 w-full py-2 rounded-sm mb-3 font-medium'>
+                <button
+                    className='flex items-center justify-center gap-2 border-2 border-blue-300 text-blue-400 w-full py-2 rounded-sm mb-3 font-medium cursor-pointer'
+                    onClick={handleGoogleSignin}
+                >
                     <FaGoogle />
                     Login with Google
                 </button>
-                
-                <button className='flex items-center justify-center gap-2 border-2 w-full py-2 rounded-sm font-medium'>
+
+                <button
+                    onClick={handleGithubSignin}
+                    className='flex items-center justify-center gap-2 border-2 w-full py-2 rounded-sm font-medium cursor-pointer'
+                >
                     <FaGithub />
                     Login with Github
                 </button>
